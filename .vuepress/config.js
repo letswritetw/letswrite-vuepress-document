213cf8104aeb5c3cacb474793d363fd5aa1cedef
Let's Write - Augustus <evilkim25@gmail.com>
2021-04-03 13:12:12 +0800

    config.js 基本設定 完成

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Let's Write VuePress 範例",
+  description: "由 Let's Write 製作的 VuePress Demo 頁面，歡迎多多分享~",
+  head: [
+    ['link', { rel: 'icon', href: 'https://letswritetw.github.io/letswritetw/dist/img/logo_512.png' }]
+  ],
+  dest: 'output',
+
+  themeConfig: {
+    logo: 'https://www.letswrite.tw/wp-content/uploads/2020/08/logo_120.svg',
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+---
+title: Let's Write VuePress
+---
+# Let's Write VuePress 範例頁面
+
+- [用 VuePress 製作說明文件頁面 – 1：安裝](https://www.letswrite.tw/vuepress-document-setup/)
+- [用 VuePress 製作說明文件頁面 – 2：config.js 基本設定](https://www.letswrite.tw/vuepress-document-basic/)
